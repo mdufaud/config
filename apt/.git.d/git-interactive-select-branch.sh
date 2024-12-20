47c6e3c99abe8d2f9005235b9583dcf3d01653e2
mdufaud <maxence_dufaud@hotmail.fr>
2024-12-20 17:15:16 +0100

    did not select properly branches

--- apt/.git.d/git-interactive-select-branch.sh
+++ apt/.git.d/git-interactive-select-branch.sh
@@ -2,16 +2,16 @@
 
 source "$HOME/.git.d/git-interactive-init.sh"
 
-bash "$__fzf_git" --list branches |
-_fzf_git_fzf --ansi \
-  --border-label '🌲 Branches' \
-  --header-lines 2 \
-  --tiebreak begin \
-  --preview-window down,border-top,40% \
-  --color hl:underline,hl+:underline \
-  --no-hscroll \
-  --bind 'ctrl-/:change-preview-window(down,70%|hidden|)' \
-  --bind "ctrl-o:execute-silent:bash \"$__fzf_git\" --list branch {}" \
-  --bind "alt-a:change-border-label(🌳 All branches)+reload:bash \"$__fzf_git\" --list all-branches" \
-  --preview "git log --oneline --graph --date=short --color=$(__fzf_git_color .) --pretty='format:%C(auto)%cd %h%d %s' \$(sed s/^..// <<< {} | cut -d' ' -f1) --" "$@" |
-sed 's/^..//' | cut -d' ' -f1
+bash "$__fzf_git" --list branches \
+  | _fzf_git_fzf --ansi \
+    --border-label '🌲 Branches' \
+    --header-lines 2 \
+    --tiebreak begin \
+    --preview-window down,border-top,40% \
+    --color hl:underline,hl+:underline \
+    --no-hscroll \
+    --bind 'ctrl-/:change-preview-window(down,70%|hidden|)' \
+    --bind "ctrl-o:execute-silent:bash \"$__fzf_git\" --list branch {}" \
+    --bind "alt-a:change-border-label(🌳 All branches)+reload:bash \"$__fzf_git\" --list all-branches" \
+    --preview "git log --oneline --graph --date=short --color=$(__fzf_git_color .) --pretty='format:%C(auto)%cd %h%d %s' \$(sed s/^..// <<< {} | cut -d' ' -f1) --" "$@" \
+    | awk '{print ($1 == "*" ? $2 : $1)}'
